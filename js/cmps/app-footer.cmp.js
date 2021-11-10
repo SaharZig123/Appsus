1b9d06cddf542d0b790eb404d345320200eed630
Sahar zigdon <saharzigdon123@gmail.com>
2021-11-10 10:24:16 +0200

    first

---
+++ js/cmps/app-footer.cmp.js
@@ -0,0 +1,9 @@
+import { eventBus } from "../services/event-bus-service.js"
+
+export default {
+    template: `
+        <footer class="app-footer">
+            <p>&copy; Coffeerights 2021</p>
+        </footer>
+    `,
+}
---
+++ js/routes.js
@@ -0,0 +1,14 @@
+
+
+
+const routes = [
+  {
+    path: '/',
+    component: homePage,
+  },
+  {
+    path: '/about',
+    component: aboutPage,
+  }]
+
+export const router = new VueRouter({ routes })
